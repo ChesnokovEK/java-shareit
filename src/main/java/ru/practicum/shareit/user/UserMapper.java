d665fdf107728c101339cc206722dd3bee4f928d
4ec <89256092+ChesnokovEK@users.noreply.github.com>
2024-10-25 17:41:18 +0300

    feat: Создан функционал бронирования (#2)
    * feat: Создан функционал бронирования
    
    * fix: тесты гита
    
    * fix: тесты гита
    
    * fix: тесты постмана
    
    * fix: Исправление замечаний после ревью
    
    ---------
    
    Co-authored-by: ChesnokovEK <4echoko@gmail.com>

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,22 +1,25 @@
 package ru.practicum.shareit.user;
 
 import lombok.experimental.UtilityClass;
-import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserDTO;
+import ru.practicum.shareit.user.model.User;
 
 @UtilityClass
 public class UserMapper {
 
-    public static UserDto toUserDto(User user) {
-        return new UserDto(
-                user.getId(),
-                user.getName(),
-                user.getEmail());
+    public UserDTO toUserDto(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
     }
 
-    public static User toUser(UserDto userDto, Long id) {
-        return new User(
-                id,
-                userDto.getName(),
-                userDto.getEmail());
+    public User toUser(UserDTO userDTO) {
+        return User.builder()
+                .id(userDTO.getId())
+                .name(userDTO.getName())
+                .email(userDTO.getEmail())
+                .build();
     }
 }
